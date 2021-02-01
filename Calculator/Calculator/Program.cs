3e4340ca60dfcef8d648339cf4ccc7311ff4ab6d
Ravindi Kumarasinghe <57246252+ravindikumarasinghe@users.noreply.github.com>
2021-02-01 23:10:34 +0530

    Update program.cs

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -14,9 +14,7 @@ namespace Calculator
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            
         }
     }
 }
